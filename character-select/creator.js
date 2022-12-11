d400884009f764d41133fa3d8790110050a8bfee
skylar <skylarcaulfeild@gmail.com>
2022-12-11 01:38:02 -0600

    added charcter creator saving

--- character-select/creator.js
+++ character-select/creator.js
@@ -1,3 +1,74 @@
 window.onload = function() {
+    //Creates Variables we will use later
+    var skills = document.querySelector("#Start-Skill-Point");
+    var luckskill = document.querySelector('#luck-skill');
+    var speechskill = document.querySelector('#speech-skill');
+    var strengthskill = document.querySelector('#strength-skill');
+    var intelligenceskill = document.querySelector('#intelligence-skill');
+    var createbutton = document.querySelector('#create-button');
+    createbutton.onclick = function() {charctersave()};
+    //set default number of skill points 
+    skills.innerText = 15;
+    var skillnumber = 15;
+    //Creates Event Listeners 
+    luckskill.addEventListener('change', function() {
+        console.log('event was triggered');
+        reload();
+    });
+    speechskill.addEventListener('change', function() {
+        reload();
+    });
+    strengthskill.addEventListener('change', function() {
+        reload();
+    });
+    intelligenceskill.addEventListener('change',function() {
+        reload();
+    });
+}
+//Calculates what should be displayed in the skill meter
+function reload() {
+    var luckskill = document.querySelector('#luck-skill').value;
+    var speechskill = document.querySelector('#speech-skill').value;
+    var strengthskill = document.querySelector('#strength-skill').value;
+    var intelligenceskill = document.querySelector('#intelligence-skill').value;
+    var skills = document.querySelector('#Start-Skill-Point');    
+    var inputs = document.querySelector("#input-selector");
+    skills.innerText =  15 - luckskill - speechskill - strengthskill - intelligenceskill;
+    if (skills.innerText === "0") {
+        document.querySelector('#luck-skill').classList.add("disabled");
+        document.querySelector('#speech-skill').classList.add('disabled');
+        document.querySelector('#strength-skill').classList.add('disabled');
+        document.querySelector('#intelligence-skill').classList.add('disabled');
+    }
+}
+function charctersave() {
+    var skillnumber = document.querySelector('#Start-Skill-Point'); 
+    if (skillnumber.innerText !== "0") {
+        window.alert("Please Select more skills");
+        return;
+    }
+    if (JSON.parse(localStorage.getItem('charcters')) === null) {
+        console.log('debug function');
+        var charcters = [];
+    } 
+    else {
+        var charcters = JSON.parse(localStorage.getItem('charcters'));
+        console.log('debug')
+    }
+    var charctername = document.querySelector('#character-name').value;
+    var luckskill = document.querySelector('#luck-skill').value;
+    var speechskill = document.querySelector('#speech-skill').value;
+    var strengthskill = document.querySelector('#strength-skill').value;
+    var intelligenceskill = document.querySelector('#intelligence-skill').value;
+    var idnumber = Math.floor(Math.random() * 100) + 1;
+    charcters.push({name: charctername, 
+        luckskills: luckskill, 
+        speechskills: speechskill, 
+        strengthskills: strengthskill, 
+        intelligenceskills: intelligenceskill, 
+        idnumbers: idnumber
+    });
+    localStorage.setItem('charcters', JSON.stringify(charcters));
     
+
 }
